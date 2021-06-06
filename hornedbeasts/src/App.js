780e8a97c0f6cb24ad326b28632646d96e0797ab
YousefAbuJalboush <yousef.y.jalboush@gmail.com>
2021-06-07 01:14:55 +0300

    add files

---
+++ hornedbeasts/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Main from './component/Main';
+
+
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Main/>
+        <Footer/>
+
+      </div>
+
+    )
+  }
+}
+export default App;
