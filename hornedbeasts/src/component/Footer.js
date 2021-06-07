cd5d794c1aa088f6668400acd5666281d088a4c5
YousefAbuJalboush <yousef.y.jalboush@gmail.com>
2021-06-07 17:10:00 +0300

    Add Lap-Class02

--- hornedbeasts/src/component/Footer.js
+++ hornedbeasts/src/component/Footer.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-
 class Footer extends React.Component {
 
-    render(){
-        return(
-            <div>
-               
-                <h1>Yousef Abu-Jalboush</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Yousef Abu-Jalboush</h1>
+      </div>
+    );
+  }
 }
-export default Footer
+export default Footer;
