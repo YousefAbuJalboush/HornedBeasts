8d34c92edc824bbc145eed1bf0620547a72881cc
YousefAbuJalboush <yousef.y.jalboush@gmail.com>
2021-06-08 14:13:04 +0300

    add react Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Header from './component/Header';
 import Footer from './component/Footer';
 import Main from './component/Main';
