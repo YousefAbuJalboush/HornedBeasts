ae6ea10a7deeee8e852a43a283f98f1a11050450
YousefAbuJalboush <yousef.y.jalboush@gmail.com>
2021-06-09 14:10:09 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './component/Header';
 import Footer from './component/Footer';
 import Main from './component/Main';
+import hornedBeasts from './component/data.json';
 
-
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  render() {
     return (
       <div>
-        <Header/>
-        <Main/>
-        <Footer/>
+        <Header />
+        <Main
+          hornedBeasts={hornedBeasts}
+        />
+        <Footer />
       </div>
 
     );
