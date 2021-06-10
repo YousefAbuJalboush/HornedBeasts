b6a90ef9efb4ccdd063cd16617c27772730f3aa9
YousefAbuJalboush <yousef.y.jalboush@gmail.com>
2021-06-10 14:38:10 +0300

    test

--- src/component/Header.js
+++ src/component/Header.js
@@ -11,12 +11,12 @@ class Header extends React.Component {
 
       <Navbar collapseOnSelect expand="xxl" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">HornedBeasts</Navbar.Brand>
+          <Navbar.Brand href="#home">Horned Beasts</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#features">Home</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="#pricing"> . . . . </Nav.Link>
               <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
